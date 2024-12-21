8268ab3c2acca826fe6a698262100e02f32df1b7
frchp <achille.petitjean@gmail.com>
2024-12-21 20:57:43 +0100

    Rework tree structure

--- cmake/include_list.cmake
+++ cmake/include_list.cmake
@@ -1,11 +1,18 @@
 message("include_list.cmake")
 
 set(include_list ${include_list}
+${PROJ_PATH}/src/app/intensity_sensor
+${PROJ_PATH}/src/app/motor_controller
+${PROJ_PATH}/src/app/order_handler
+${PROJ_PATH}/src/app/speed_sensor
+${PROJ_PATH}/src/bsp/adc
+${PROJ_PATH}/src/bsp/clock
+${PROJ_PATH}/src/bsp/gpio
+${PROJ_PATH}/src/bsp/system
+${PROJ_PATH}/src/bsp/timer_counter
+${PROJ_PATH}/src/bsp/timer_pwm
+${PROJ_PATH}/src/tasks
 ${PROJ_PATH}/src
-${PROJ_PATH}/src/clock
-${PROJ_PATH}/src/gpio
-${PROJ_PATH}/src/system
-${PROJ_PATH}/src/timer
 ${PROJ_PATH}/inc
 # vendor
 ${PROJ_PATH}/vendor/Drivers/CMSIS/Core/Include
