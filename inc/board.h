dca0f6a60a15e57fc1cb784a7b200a9037043865
frchp <achille.petitjean@gmail.com>
2024-12-21 21:18:28 +0100

    Misc : add vendors files to compile list, add board file, add new todo app file

---
+++ inc/board.h
@@ -0,0 +1,13 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+//! List the GPIO connexions
+#define BOARD_GPIO_MTR_CTRL
+#define BOARD_GPIO_MTR_FB
+#define BOARD_GPIO_MTR_DIR
+
+//! List the ADC connexions
+#define BOARD_ADC_CURRENT
+#define BOARD_ADC_POWER
+
+#endif // _BOARD_H_
