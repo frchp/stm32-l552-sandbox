a4f031b937cd3dd6573885f4cc2c59c42ce6d038
frchp <achille.petitjean@gmail.com>
2025-01-28 15:46:06 +0100

    Create reg_macro.h

---
+++ src/utils/reg_macro.h
@@ -0,0 +1,15 @@
+#ifndef _REG_MACRO_H_
+#define _REG_MACRO_H_
+
+/**
+ * Macros to handle register values easily
+ */
+#define SET_BIT(REG, BIT)     ((REG) |= (BIT))
+#define CLEAR_BIT(REG, BIT)   ((REG) &= ~(BIT))
+#define READ_BIT(REG, BIT)    ((REG) & (BIT))
+#define CLEAR_REG(REG)        ((REG) = (0x0))
+#define WRITE_REG(REG, VAL)   ((REG) = (VAL))
+#define READ_REG(REG)         ((REG))
+#define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
+#endif // _REG_MACRO_H_
