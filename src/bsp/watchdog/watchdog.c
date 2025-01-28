3a6b33aa850d0d11ff338e5a658d34b462b703da
frchp <achille.petitjean@gmail.com>
2025-01-28 15:46:33 +0100

    Watchdog without LL

--- src/bsp/watchdog/watchdog.c
+++ src/bsp/watchdog/watchdog.c
@@ -2,8 +2,26 @@
 
 #include "watchdog.h"
 
+#include "reg_macro.h"
+#include "clocks.h"
+
 #include "stm32l5xx_ll_iwdg.h"
 #include "stm32l5xx_ll_rcc.h"
+
+#define IWDG_KEY_RELOAD                 0x0000AAAAU
+#define IWDG_KEY_ENABLE                 0x0000CCCCU
+#define IWDG_KEY_WR_ACCESS_ENABLE       0x00005555U
+#define IWDG_KEY_WR_ACCESS_DISABLE      0x00000000U
+
+#define IWDG_PRESCALER_4                0x00000000U                           /*!< Divider by 4   */
+#define IWDG_PRESCALER_8                (IWDG_PR_PR_0)                        /*!< Divider by 8   */
+#define IWDG_PRESCALER_16               (IWDG_PR_PR_1)                        /*!< Divider by 16  */
+#define IWDG_PRESCALER_32               (IWDG_PR_PR_1 | IWDG_PR_PR_0)         /*!< Divider by 32  */
+#define IWDG_PRESCALER_64               (IWDG_PR_PR_2)                        /*!< Divider by 64  */
+#define IWDG_PRESCALER_128              (IWDG_PR_PR_2 | IWDG_PR_PR_0)         /*!< Divider by 128 */
+#define IWDG_PRESCALER_256              (IWDG_PR_PR_2 | IWDG_PR_PR_1)         /*!< Divider by 256 */
+
+#define IWDG_PERIOD_CNT                 (0xFEE) /* 500 ms */
 
 /**
   @brief Setup the Watchdog.
@@ -15,16 +33,15 @@ void Watchdog_Activate(void)
   {
     loc_bInitialized = true;
     /* Enable clock feeding IWDG (32kHz) */
-    LL_RCC_LSI_Enable();
-    while (!LL_RCC_LSI_IsReady());
+    Clocks_EnableLSI();
 
     /* Configure the IWDG with window option disabled */
-    LL_IWDG_Enable(IWDG);                             // Enable the IWDG by writing 0x0000 CCCC in the IWDG_KR register
-    LL_IWDG_EnableWriteAccess(IWDG);                  // Enable register access by writing 0x0000 5555 in the IWDG_KR register
-    LL_IWDG_SetPrescaler(IWDG, LL_IWDG_PRESCALER_4);  // Write the IWDG prescaler by programming IWDG_PR from 0 to 7 - LL_IWDG_PRESCALER_4 (0) is lowest divider
-    LL_IWDG_SetReloadCounter(IWDG, 0xFEE); // 500ms   // Write the reload register (IWDG_RLR)
-    while (!LL_IWDG_IsReady(IWDG));                   // Wait for the registers to be updated (IWDG_SR = 0x0000 0000)
-    LL_IWDG_ReloadCounter(IWDG);                      // Refresh the counter value with IWDG_RLR (IWDG_KR = 0x0000 AAAA)
+    WRITE_REG(IWDG->KR, IWDG_KEY_ENABLE);             // Enable the IWDG by writing 0x0000 CCCC in the IWDG_KR register
+    WRITE_REG(IWDG->KR, IWDG_KEY_WR_ACCESS_ENABLE);   // Enable register access by writing 0x0000 5555 in the IWDG_KR register
+    WRITE_REG(IWDG->PR, IWDG_PR_PR & IWDG_PRESCALER_4); // Write the IWDG prescaler by programming IWDG_PR from 0 to 7 - IWDG_PRESCALER_4 (0) is lowest divider
+    WRITE_REG(IWDG->RLR, IWDG_RLR_RL & IWDG_PERIOD_CNT);// Write the reload register
+    while(READ_BIT(IWDG->SR, IWDG_SR_PVU | IWDG_SR_RVU | IWDG_SR_WVU) != 0U);
+    Watchdog_Refresh();
   }
 }
 
@@ -33,5 +50,5 @@ void Watchdog_Activate(void)
  */
 void Watchdog_Refresh(void)
 {
-  LL_IWDG_ReloadCounter(IWDG);
+  WRITE_REG(IWDG->KR, LL_IWDG_KEY_RELOAD);
 }
